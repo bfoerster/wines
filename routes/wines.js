443d9323226811d324414605962fadb795434f9e
Bernhard <f_oobar@yahoo.de>
2017-02-14 16:22:21 +0100

    Added eslint and fixed style errors

--- routes/wines.js
+++ routes/wines.js
@@ -2,7 +2,6 @@
 
 module.exports = function (server) {
     server.get('/wines', function(request, response, next){
-        console.log("GET all wines");
         response.send(200);
         return next();
     });
